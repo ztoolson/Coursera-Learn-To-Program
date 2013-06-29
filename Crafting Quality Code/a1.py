daa663fe931acb42c54eae4e4357325603d8b13d
Zach Toolson <zach.toolson@gmail.com>
2013-06-29 13:11:56 -0600

    first commit

--- Crafting Quality Code/a1.py
+++ Crafting Quality Code/a1.py
@@ -9,6 +9,8 @@ def num_buses(n):
     >>> num_buses(75)
     2
     """
+    # Minimum number of buses requires is 1 based off precondition
+    return 1 + (n // 50)
 
 
 def stock_price_summary(price_changes):
@@ -21,6 +23,16 @@ def stock_price_summary(price_changes):
     >>> stock_price_summary([0.01, 0.03, -0.02, -0.14, 0, 0, 0.10, -0.01])
     (0.14, -0.17)
     """
+    sum_of_gains = 0
+    sum_of_losses = 0
+
+    for element in price_changes:
+        if element > 0:
+            sum_of_gains += element #gains are positive changes
+        else:
+            sum_of_losses += element #losses are negative changes
+
+    return (sum_of_gains, sum_of_losses)
 
 
 def swap_k(L, k):
@@ -35,7 +47,15 @@ def swap_k(L, k):
     >>> nums
     [5, 6, 3, 4, 1, 2]
     """
+    # Create a list that swaps the elements
+    #copy_list = end elements + middle elements + beginning elements 
+    copy_list = L[-k:] + L[k:-k] + L[:k] 
 
+    #update the original list L
+    for i in range(len(L)):
+        L[i] = copy_list[i]
+        
+    
 
 if __name__ == '__main__':
     import doctest
